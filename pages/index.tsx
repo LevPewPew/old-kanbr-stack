af92eb03b3cf4dfb79e7691d95e5904ca8fe7600
LevPewPew <levthedev@protonmail.com>
2022-07-07 22:04:22 +1000

    updated all packages

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { GetStaticProps } from 'next';
 import Layout from 'components/Layout';
 import Post, { PostProps } from 'components/Post';
-import prisma from 'prisma/client';
+import prisma from 'lib/prisma';
 
 export const getStaticProps: GetStaticProps = async () => {
   const feed = await prisma.post.findMany({
@@ -34,20 +34,6 @@ const Blog: React.FC<Props> = (props) => {
           ))}
         </main>
       </div>
-      <style jsx>{`
-        .post {
-          background: white;
-          transition: box-shadow 0.1s ease-in;
-        }
-
-        .post:hover {
-          box-shadow: 1px 1px 3px #aaa;
-        }
-
-        .post + .post {
-          margin-top: 2rem;
-        }
-      `}</style>
     </Layout>
   );
 };
--- pages/p/[id].tsx
+++ pages/p/[id].tsx
@@ -3,7 +3,7 @@ import { GetServerSideProps } from 'next';
 import ReactMarkdown from 'react-markdown';
 import Layout from 'components/Layout';
 import { PostProps } from 'components/Post';
-import prisma from 'prisma/client';
+import prisma from 'lib/prisma';
 
 export const getServerSideProps: GetServerSideProps = async ({ params }) => {
   const post = await prisma.post.findUnique({
@@ -35,27 +35,6 @@ const Post: React.FC<PostProps> = (props) => {
         <p>By {props?.author?.name || 'Unknown author'}</p>
         <ReactMarkdown children={props.content} />
       </div>
-      <style jsx>{`
-        .page {
-          background: white;
-          padding: 2rem;
-        }
-
-        .actions {
-          margin-top: 2rem;
-        }
-
-        button {
-          background: #ececec;
-          border: 0;
-          border-radius: 0.125rem;
-          padding: 1rem 2rem;
-        }
-
-        button + button {
-          margin-left: 1rem;
-        }
-      `}</style>
     </Layout>
   );
 };
