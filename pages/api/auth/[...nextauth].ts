f1aafd731b5ad0ae86882169ee2958dc692798a6
LevPewPew <levthedev@protonmail.com>
2022-07-11 16:02:05 +1000

    tons of stuff, woops

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,20 @@
+import { NextApiHandler } from 'next';
+import NextAuth from 'next-auth';
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import GitHubProvider from 'next-auth/providers/github';
+import prisma from '../../../lib/prisma';
+
+const options = {
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+  ],
+  adapter: PrismaAdapter(prisma),
+  secret: process.env.SECRET,
+};
+
+export default function authHandler(req, res): NextApiHandler {
+  return NextAuth(req, res, options);
+}
---
+++ pages/api/publish/[id].ts
@@ -0,0 +1,8 @@
+// NEXT figure out next Request types
+// import { Request } from 'next';
+import prisma from '../../../lib/prisma';
+
+// PUT /api/publish/:id
+export default async function handle(req, res) {
+  const { id } = req.query;
+}
