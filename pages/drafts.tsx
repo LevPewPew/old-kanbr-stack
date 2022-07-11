ba2a155413fdae21f6e8e508f3bd4a4160ca9cbb
LevPewPew <levthedev@protonmail.com>
2022-07-11 17:02:36 +1000

    added delete to post

--- pages/drafts.tsx
+++ pages/drafts.tsx
@@ -23,6 +23,7 @@ export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
       },
     },
   });
+
   return {
     props: { drafts },
   };
--- pages/p/[id].tsx
+++ pages/p/[id].tsx
@@ -31,6 +31,13 @@ async function publishPost(id: string): Promise<void> {
   await Router.push('/');
 }
 
+async function deletePost(id: string): Promise<void> {
+  await fetch(`/api/post/${id}`, {
+    method: 'DELETE',
+  });
+  Router.push('/');
+}
+
 const Post: React.FC<PostProps> = (props) => {
   const { data: session, status } = useSession();
   if (status === 'loading') {
@@ -51,6 +58,9 @@ const Post: React.FC<PostProps> = (props) => {
         <ReactMarkdown children={props.content} />
         {!props.published && userHasValidSession && postBelongsToUser && (
           <button onClick={() => publishPost(props.id)}>Publish</button>
+        )}
+        {userHasValidSession && postBelongsToUser && (
+          <button onClick={() => deletePost(props.id)}>Delete</button>
         )}
       </div>
       <style jsx>{`
