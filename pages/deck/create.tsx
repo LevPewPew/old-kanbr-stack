43853f5a1dba76dc6f8c02f6c4e502fa8ea043d0
LevPewPew <levthedev@protonmail.com>
2022-07-27 11:47:58 +1000

    updated create ticket page placeholder

---
+++ pages/deck/create.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Layout from 'components/Layout';
+
+interface Props {}
+
+export default function CreateTicket(props: Props) {
+  return (
+    <Layout>
+      <div className="page">
+        <h1>CREATE TICKET PAGE PLACEHOLDER</h1>
+      </div>
+    </Layout>
+  );
+}
