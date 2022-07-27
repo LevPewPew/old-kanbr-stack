088a1e2192e9f5c93920833dac2dc63131f051f3
LevPewPew <levthedev@protonmail.com>
2022-07-27 17:05:54 +1000

    added zod schema validation for forms

--- components/create-card-form/CreateCardForm.tsx
+++ components/create-card-form/CreateCardForm.tsx
@@ -7,7 +7,12 @@ import { Card } from '@prisma/client';
 import { z } from 'zod';
 import { trpc } from 'utils/trpc';
 
-const schema = z.object({ title: z.string(), description: z.string().nullish() });
+const schema = z.object({
+  title: z.string(), // FIXME make this like the schema object in the trpc end?
+  description: z.string().nullish(),
+});
+
+type Schema = z.infer<typeof schema>;
 
 export default function CreateCardForm() {
   const createCard = trpc.useMutation(['createCard']);
@@ -16,11 +21,19 @@ export default function CreateCardForm() {
     handleSubmit,
     register,
     formState: { errors, isSubmitting },
-  } = useForm({ resolver: zodResolver(schema) });
+  } = useForm<Schema>({ resolver: zodResolver(schema) });
 
-  function onSubmit(values: any) {
+  function onSubmit(values: Schema) {
+    console.log(values);
+    // const foo = {
+    //   title: '',
+    //   description: 'ha',
+    // };
+    //@ts-ignore
     createCard.mutate(values);
   }
+  //@ts-ignore
+  // const onSubmit = (data) => console.log(data);
 
   useEffect(() => {
     if (createCard.isSuccess) {
@@ -33,17 +46,22 @@ i.e. trpc also uses a zod object, can probably share between the FC and the form
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      <FormControl isInvalid={Boolean(errors.title)}>
-        <FormLabel htmlFor="title">Title</FormLabel>
-        <Input id="title" placeholder="title" {...register('title')} />
-        <FormErrorMessage>{errors.title && 'required!'}</FormErrorMessage>
-        <FormLabel htmlFor="description">Description</FormLabel>
-        <Input id="description" placeholder="description" {...register('description')} />
-        <FormErrorMessage>{errors.description && "i don't even know..."}</FormErrorMessage>
-      </FormControl>
+      {/* <FormControl isInvalid={Boolean(errors.title)}> */}
+      <FormLabel htmlFor="title">Title</FormLabel>
+      <Input id="title" placeholder="title" {...register('title')} />
+      {/* <FormErrorMessage>{errors.title?.message}</FormErrorMessage> */}
+      {errors.title?.message && <p>{errors.title?.message}</p>}
+      <FormLabel htmlFor="description">Description</FormLabel>
+      <Input id="description" placeholder="description" {...register('description')} />
+      <FormErrorMessage>{errors.description?.message}</FormErrorMessage>
+      {/* </FormControl> */}
       <Button mt={4} colorScheme="teal" isLoading={isSubmitting} type="submit">
         Submit
       </Button>
     </form>
   );
 }
+
+/* FIXME: form sends empty string for empty values instead of null
+https://github.com/react-hook-form/react-hook-form/issues/656
+ */
