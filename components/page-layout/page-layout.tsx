d605634f6802aa2a392008bdb6e37c0dadfce8af
LevPewPew <levthedev@protonmail.com>
2022-07-29 18:50:43 +1000

    removed vercel tutorial tech debt from page layout and made the layout better

--- components/page-layout/page-layout.tsx
+++ components/page-layout/page-layout.tsx
@@ -1,50 +1,16 @@
 import React, { ReactNode } from 'react';
+import { Container } from '@chakra-ui/react';
 import { Header } from 'components';
 
 type Props = {
   children: ReactNode;
 };
 
-/* FIXME clean up Vercel project tutorial tech debt */
 export default function Layout(props: Props) {
   return (
     <div>
       <Header />
-      <div className="layout">{props.children}</div>
-      <style jsx global>{`
-        html {
-          box-sizing: border-box;
-        }
-
-        *,
-        *:before,
-        *:after {
-          box-sizing: inherit;
-        }
-
-        body {
-          margin: 0;
-          padding: 0;
-          font-size: 16px;
-          font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial,
-            sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol';
-          background: rgba(0, 0, 0, 0.05);
-        }
-
-        input,
-        textarea {
-          font-size: 16px;
-        }
-
-        button {
-          cursor: pointer;
-        }
-      `}</style>
-      <style jsx>{`
-        .layout {
-          padding: 0 2rem;
-        }
-      `}</style>
+      <Container>{props.children}</Container>
     </div>
   );
 }
