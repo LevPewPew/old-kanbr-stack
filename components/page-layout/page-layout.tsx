d4db46f7c2c283fd5c164e311bba83d46720c533
LevPewPew <levthedev@protonmail.com>
2022-08-02 10:24:12 +1000

    lifted conent of left and right nav components to DRY and take advantage of composition

--- components/page-layout/page-layout.tsx
+++ components/page-layout/page-layout.tsx
@@ -2,21 +2,113 @@ import React, { ReactNode } from 'react';
 import { useRouter } from 'next/router';
 import Link from 'next/link';
 import { signOut, useSession } from 'next-auth/react';
-import { Container } from '@chakra-ui/react';
+import { Avatar, Container } from '@chakra-ui/react';
 import { Header } from '~/components';
-import LeftNavigation from './left-navigation';
-import RightNavigation from './right-navigation';
+import { isRouteActive } from '~/helpers';
 
 type Props = {
   children: ReactNode;
 };
 
+type SessionStatus = 'authenticated' | 'loading' | 'unauthenticated';
+
+interface LinkModel {
+  id: string;
+  text: string;
+  href: string;
+  displayStatus?: SessionStatus;
+}
+
+const leftLinks: LinkModel[] = [
+  {
+    id: '1',
+    text: 'Home',
+    href: '/',
+  },
+  {
+    id: '2',
+    text: 'Projects',
+    href: '/projects',
+    displayStatus: 'authenticated',
+  },
+  {
+    id: '3',
+    text: 'Decks',
+    href: '/projects/decks',
+    displayStatus: 'authenticated',
+  },
+  {
+    id: '4',
+    text: 'Cards',
+    href: '/projects/decks/cards',
+    displayStatus: 'authenticated',
+  },
+  {
+    id: '5',
+    text: 'New Card',
+    href: '/projects/decks/cards/create',
+    displayStatus: 'authenticated',
+  },
+];
+
+const rightLinks: LinkModel[] = [
+  {
+    id: '1',
+    text: 'Log in',
+    href: '/api/auth/signin',
+    displayStatus: 'unauthenticated',
+  },
+];
+
 /* FIXME make more composable by lifting left and right to props. This will
 allow pages to control what is in navigation */
 export default function PageLayout(props: Props) {
+  const { status: sessionStatus } = useSession();
+  const router = useRouter();
+
   return (
     <div>
-      <Header left={<LeftNavigation />} right={<RightNavigation />} />
+      <Header
+        left={leftLinks.map((link) => {
+          if (link.displayStatus === sessionStatus) {
+            return (
+              // TODO DRY the Link and anchor into an opinionated link, with variant prop
+              <Link key={link.id} href={link.href}>
+                <a className="bold" data-active={isRouteActive(link.href, router)}>
+                  {link.text}
+                </a>
+              </Link>
+            );
+          }
+        })}
+        right={rightLinks.map((link) => {
+          if (link.displayStatus === sessionStatus) {
+            return (
+              // TODO DRY the Link and anchor into an opinionated link, with variant prop
+              <>
+                <Link key={link.id} href={link.href}>
+                  <a className="bold" data-active={isRouteActive(link.href, router)}>
+                    {link.text}
+                  </a>
+                </Link>
+              </>
+            );
+          }
+          if (sessionStatus === 'authenticated') {
+            return (
+              <>
+                <button onClick={() => signOut()}>
+                  <a>Log out</a>
+                </button>
+                <Avatar />
+              </>
+            );
+          }
+          if (sessionStatus === 'loading') {
+            return <div>Checking authorization...</div>;
+          }
+        })}
+      />
       <Container>{props.children}</Container>
     </div>
   );
