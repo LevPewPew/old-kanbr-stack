123d21aa3ad2bfca0003c7b74b6ea106f9f9786d
LevPewPew <levthedev@protonmail.com>
2022-08-27 14:54:27 +1000

    removed button that was meant to be on a different page

--- src/pages/projects/[id].tsx
+++ src/pages/projects/[id].tsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import { Button } from '~/components';
 
 export default function ProjectPage() {
   return (
     <div>
       <h1>PROJECT PAGE PLACEHOLDER</h1>
-      <Button onClick={() => alert('created a deck')}>Create Deck</Button>
     </div>
   );
 }
