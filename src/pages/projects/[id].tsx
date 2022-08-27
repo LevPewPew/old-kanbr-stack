20ccf6fd000dfd5c29385feafd4477208c16da7b
LevPewPew <levthedev@protonmail.com>
2022-08-27 14:28:13 +1000

    updated ProjectPage placeholder with dummy button

--- src/pages/projects/[id].tsx
+++ src/pages/projects/[id].tsx
@@ -1,5 +1,11 @@
 import React from 'react';
+import { Button } from '~/components';
 
-export default function Foobar() {
-  return <h1>PROJECT PAGE PLACEHOLDER</h1>;
+export default function ProjectPage() {
+  return (
+    <div>
+      <h1>PROJECT PAGE PLACEHOLDER</h1>
+      <Button onClick={() => alert('created a deck')}>Create Deck</Button>
+    </div>
+  );
 }
