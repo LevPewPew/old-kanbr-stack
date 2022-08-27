bcf2251f3d1a94dcad348245609ff3fa3866a372
LevPewPew <levthedev@protonmail.com>
2022-08-27 14:13:36 +1000

    created deck create route

---
+++ src/server/routers/decks.ts
@@ -0,0 +1,21 @@
+import { z } from 'zod';
+import { createRouter } from '~/server/create-router';
+
+export const createDeckSchema = z.object({
+  title: z.string().min(1, { message: 'Required' }),
+  description: z.string().nullish(),
+  projectId: z.string(),
+});
+
+export const decksRouter = createRouter().mutation('create', {
+  input: createDeckSchema,
+  async resolve({ ctx, input }) {
+    const deck = await ctx.prisma.deck.create({
+      data: input,
+    });
+
+    return deck;
+  },
+});
+
+export type CardsRouter = typeof decksRouter;
