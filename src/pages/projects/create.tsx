a0ad106456eff2f93e8c9904369371aecdb10ce0
LevPewPew <levthedev@protonmail.com>
2022-08-27 14:55:40 +1000

    added project form to a new create project page

---
+++ src/pages/projects/create.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { PageLayout, ProjectForm } from '~/components';
+
+export default function CreateProjectPage() {
+  return (
+    <PageLayout>
+      <ProjectForm />
+    </PageLayout>
+  );
+}
