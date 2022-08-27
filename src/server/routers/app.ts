49ad7f2728804f85fb13d11192bdd2af2241fc71
LevPewPew <levthedev@protonmail.com>
2022-08-27 14:48:44 +1000

    added new routers to root router

--- src/server/routers/app.ts
+++ src/server/routers/app.ts
@@ -1,12 +1,16 @@
 import { transformer } from '~/server/transformer';
 import { createRouter } from '~/server/create-router';
 import { cardsRouter } from './cards';
+import { decksRouter } from './decks';
 import { developerRouter } from './developer';
+import { projectsRouter } from './projects';
 import { usersRouter } from './users';
 
 export const appRouter = createRouter()
   .transformer(transformer)
   .merge('card.', cardsRouter) // FIXME change to plural
   .merge('dev.', developerRouter)
-  .merge('users.', usersRouter);
+  .merge('users.', usersRouter)
+  .merge('projects.', projectsRouter)
+  .merge('decks.', decksRouter);
 export type AppRouter = typeof appRouter;
