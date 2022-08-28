0be93c0fa3e475f94388751de0b3d3c6d0aa8437
LevPewPew <levthedev@protonmail.com>
2022-08-29 08:59:58 +1000

    added type to context of ProjectPages getServerSideProps

--- src/pages/projects/[projectId].tsx
+++ src/pages/projects/[projectId].tsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import { GetServerSidePropsContext } from 'next';
 import prisma from '~/clients/prisma';
 
-// NEXT fix the any type
-export async function getServerSideProps(ctx: any) {
+/* FIXME fix the any type. need to create a type that contains the possible 
+dynamic route variable names, or just the one to expect on ProjectPage. this 
+will easily get out of sync though, is it worth it? is there a more kept in sync
+way it can be done that just needs to be researched? */
+export async function getServerSideProps(ctx: GetServerSidePropsContext<any>) {
   const { projectId } = ctx.params;
   const decks = await prisma.deck.findMany({
     where: {
