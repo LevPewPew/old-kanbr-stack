558b36e01a662ed5c791621c48a2ed495511e821
LevPewPew <levthedev@protonmail.com>
2022-09-09 08:21:07 +1000

    converted use of lodash to radash

--- src/helpers/sanitize-react-hook-form-values/sanitize-react-hook-form-values.ts
+++ src/helpers/sanitize-react-hook-form-values/sanitize-react-hook-form-values.ts
@@ -1,12 +1,11 @@
-// TODO don't use lodash, roll own, or try the radash library
-import { pickBy } from 'lodash';
+import { shake } from 'radash';
 
 export default function sanitizeReactHookFormValues<T extends Record<string, any>>(object: T) {
   /* react-hook-form converts empty fields to have empty strings, instead of
     leaving undefined. we want undefined to be passed to the database, so we
     convert the empty strings to undefined */
-  const newObject = pickBy(object, (value) => {
-    return value !== '';
+  const newObject = shake(object, (value) => {
+    return value === '';
   }) as T;
 
   return newObject;
